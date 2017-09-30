79db4a101ea1e28eb482e03e510c0f785f62127b
Alexey Mironov <s18b3_mironov@179.ru>
2017-09-30 11:50:35 +0300

    add models an form

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import NumberRange
 
+
+class ColorMark(Form):
+    mark = StringField('Mark', validators=[NumberRange(1, 100, 'А ещё в матклассе учишься!')])
--- app/models.py
+++ app/models.py
@@ -6,4 +6,25 @@ db models
 from app import db
 
 
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_link = db.Column(db.String(128))
+    hex = db.Column(db.String(8))
 
+    marks = db.relationship('Mark', backref='color', lazy='dynamic')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    grade = db.Column(db.String(4), index=True, nullable=False)
+
+    marks = db.relationship('Mark', backref='user', lazy='dynamic')
+
+
+class Mark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mark = db.Column(db.Integer, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    color_id = db.Column(db.Integer, db.ForeignKey('color.id'), nullable=False)
