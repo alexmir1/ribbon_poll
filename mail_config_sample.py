38f066b716c80e9237ffd0bae9b229215c1a1c4f
Alexey Mironov <s18b3_mironov@179.ru>
2017-11-17 23:29:38 +0300

    add mail config sample

---
+++ mail_config_sample.py
@@ -0,0 +1,9 @@
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 's18b3_mironov@179.ru'
+MAIL_PASSWORD = '***'
+
+# administrator list
+ADMINS = ['s18b3_mironov@179.ru']
